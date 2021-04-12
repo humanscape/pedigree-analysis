2671504770ec3308e1c9537f04b4a6ee6f337ebd
humanscape-james <james@humanscape.io>
2021-04-12 15:45:08 +0900

    refactor: const file to typescript

--- lib/const/gender.ts
+++ lib/const/gender.ts
@@ -1,5 +1,11 @@
-export default {
-  FEMALE: 'XX',
-  MALE: 'XY',
-  choices: new Set(['XX', 'XY', 'X', 'XXX', 'XXY', 'XYY', 'XXYY']),
-};
+const genderList = ['XX', 'XY', 'X', 'XXX', 'XXY', 'XYY', 'XXYY'] as const;
+const choices = new Set(genderList);
+
+export type Gender = typeof genderList[number];
+
+export default Object.freeze({
+  FEMALE: 'XX' as Gender,
+  MALE: 'XY' as Gender,
+  isValid: (gender: string) => choices.has(gender as Gender),
+  isMale: (gender: Gender) => gender.endsWith('Y'),
+});
