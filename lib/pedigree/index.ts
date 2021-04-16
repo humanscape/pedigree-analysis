1826833da2a0b59f3f338977d51e2833d2cbc6da
humanscape-james <james@humanscape.io>
2021-04-16 11:07:15 +0900

    feat: pedigree class constructor

--- lib/pedigree/index.ts
+++ lib/pedigree/index.ts
@@ -1,3 +1,27 @@
-class Pedigree {}
+import { genotypes, inheritances } from '../const';
+import CommonDisease, { ParentRanges } from '../disease/common';
+import FamilyMemberFactory from '../family-member';
+
+import type { Genotype, Allele } from '../const';
+import type { FamilyMember } from '../family-member';
+
+class Pedigree {
+  _disease: CommonDisease;
+
+  _target: FamilyMember;
+
+  constructor(disease: CommonDisease, target: FamilyMember) {
+    if (!(disease instanceof CommonDisease))
+      throw new Error('diease must be instance created by DiseaseFactory.');
+    if (!FamilyMemberFactory._isInstance(target))
+      throw new Error(
+        'target must be instance created by FamilyMemberFactory.',
+      );
+
+    this._disease = disease;
+    this._target = target;
+    this._genotypes = {};
+  }
+}
 
 export default Pedigree;
