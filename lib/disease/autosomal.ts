14bee9c151f7b2bab3ef197b568b042019625234
humanscape-james <james@humanscape.io>
2021-04-16 10:25:29 +0900

    refactor: remove unused functions

--- lib/disease/autosomal.ts
+++ lib/disease/autosomal.ts
@@ -1,4 +1,3 @@
-/* eslint-disable class-methods-use-this */
 import { genotypes } from '../const';
 import type { Genotype } from '../const';
 import CommonDisease from './common';
@@ -11,29 +10,6 @@ import type { Phenotype } from './common';
 class AutosomalDisease extends CommonDisease {
   static _validGenotypes = genotypes.AUTOSOMAL;
 
-  getValidGenotypes() {
-    return AutosomalDisease._validGenotypes;
-  }
-
-  hasValidGenotypes({ genotypes: list }: { genotypes: Genotype[] }) {
-    if (!Array.isArray(list) || !list.length) return false;
-
-    const genotypeSet = new Set(list);
-    if (genotypeSet.size !== list.length) return false; // duplicate check
-
-    AutosomalDisease._validGenotypes.forEach((genotype) =>
-      genotypeSet.delete(genotype),
-    );
-    return genotypeSet.size === 0; // if not 0, it has invalid genotype
-  }
-
-  getPossibleGenotypes({ phenotype }: { phenotype: Phenotype }) {
-    const { _validGenotypes: validGenotypes } = AutosomalDisease;
-    if (typeof phenotype !== 'boolean') return validGenotypes;
-    return validGenotypes.filter(
-      (genotype: Genotype) => super.hasDisease(genotype) === phenotype,
-    );
-  }
 }
 
 export default AutosomalDisease;
--- lib/disease/x-linked.ts
+++ lib/disease/x-linked.ts
@@ -1,4 +1,3 @@
-/* eslint-disable class-methods-use-this */
 import { genotypes } from '../const';
 import type { Gender, Genotype } from '../const';
 import CommonDisease from './common';
@@ -11,46 +10,6 @@ import type { Phenotype } from './common';
 class XLinkedDisease extends CommonDisease {
   static _validGenotypes = genotypes.byGender;
 
-  getValidGenotypes({ gender }: { gender: Gender }) {
-    const list = XLinkedDisease._validGenotypes[gender];
-    if (!list) throw new Error(`gender '${gender}' is invalid.`);
-    return list;
-  }
-
-  hasValidGenotypes({
-    gender,
-    genotypes: list,
-  }: {
-    gender: Gender;
-    genotypes: Genotype[];
-  }) {
-    if (!Array.isArray(list) || !list.length) return false;
-
-    const genotypeSet = new Set(list);
-    if (genotypeSet.size !== list.length) return false;
-
-    (XLinkedDisease._validGenotypes[gender] ?? []).forEach((genotype) =>
-      genotypeSet.delete(genotype),
-    );
-    return genotypeSet.size === 0; // if not 0, has invalid genotypes
-  }
-
-  getPossibleGenotypes({
-    gender,
-    phenotype,
-  }: {
-    gender: Gender;
-    phenotype: Phenotype;
-  }) {
-    const list = this.getValidGenotypes({ gender });
-    const filter = phenotype
-      ? (genotype: Genotype) => super.hasDisease(genotype)
-      : phenotype === false
-      ? (genotype: Genotype) => !super.hasDisease(genotype)
-      : null;
-    if (filter) return list.filter(filter);
-    return list;
-  }
 }
 
 export default XLinkedDisease;
